a21469528bf430930dce71f45e0d879669d6d82f
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-05-08 09:47:30 +0300

    Hotfix runs correctly

--- src/main/java/gr/codehub/review/Main.java
+++ src/main/java/gr/codehub/review/Main.java
@@ -3,7 +3,8 @@ package gr.codehub.review;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first line");
-        int x = 8;
+        int x = 7;
         System.out.println("x = " + x);
+        System.out.println("New corrected version");
     }
 }
