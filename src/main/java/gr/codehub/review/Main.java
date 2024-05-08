ecd6efbc2640a82fdcdd5218287f86bc51c0a8de
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-05-08 09:45:49 +0300

    First lines of code

---
+++ src/main/java/gr/codehub/review/Main.java
@@ -0,0 +1,9 @@
+package gr.codehub.review;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My first line");
+        int x = 8;
+        System.out.println("x = " + x);
+    }
+}
